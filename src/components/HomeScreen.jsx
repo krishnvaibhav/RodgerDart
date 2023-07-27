71d17a4c47622a93b395e9afe4bba7c2f64cbec6
Aaron <aaron.p@bca.christuniversity.in>
2023-07-27 16:25:46 +0530

    Added homescreen

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { Image } from "react";
+
+import bagImg from "../assets/bag-2.png";
+
+import { HiOutlineBars4 } from "react-icons/hi2";
+import { BsFillHandbagFill } from "react-icons/bs";
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>Home Screen</h1>
+    <div className="p-4 pb-1 bg-red-400">
+      <div className="flex flex-row">
+        <HiOutlineBars4 size={25} />
+        {/* <img src={bagImg} style={{ width: 25 }} /> */}
+        <BsFillHandbagFill size={25} className="flex-1" />
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import App from "./App";
 import "../src/styles/style.css";
 import { StateProvider } from "./context/stateProvider";
 import reducer, { initialState } from "./context/reducer";
+import HomeScreen from "./components/HomeScreen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StateProvider reducer={reducer} initialState={initialState}>
-      <App />
+      <HomeScreen />
     </StateProvider>
   </React.StrictMode>
 );
