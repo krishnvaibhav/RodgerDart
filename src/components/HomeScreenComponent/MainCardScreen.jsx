ea1f78979d0ccce5ec62d65abd97a1443242f2c8
Aaron <aaron.p@bca.christuniversity.in>
2023-08-09 11:25:47 +0530

    FIrebase Value is working

--- src/components/HomeScreenComponent/MainCardScreen.jsx
+++ src/components/HomeScreenComponent/MainCardScreen.jsx
@@ -1,8 +1,60 @@
 import React, { useEffect, useState } from "react";
 import MainCards from "./MainCards";
 import Icons from "./ImagePath";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const MainCardScreen = (props) => {
+  const [cardVendor, setCardVendor] = useState([]);
+  const [cardItem, setCardItem] = useState([]);
+
+  const cardRef = collection(db, "vendor");
+  const itemRef = collection(db, "item");
+
+  const fetchVendorData = async () => {
+    try {
+      getDocs(cardRef).then((snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+          category: doc.data().category,
+        }));
+        setCardVendor({ vendor: data });
+      });
+
+      // console.log(data);
+    } catch (error) {
+      // Handle any potential errors here
+      console.error("Error fetching data:", error);
+    }
+    console.log(cardVendor.vendor);
+  };
+
+  fetchVendorData();
+
+  const fetchItemData = async () => {
+    try {
+      getDocs(itemRef).then((snapshot) => {
+        const data = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+          description: doc.data().description,
+          price: doc.data().price,
+          quant: doc.data().quant,
+          type: doc.data().type,
+          vid: doc.data().vid,
+        }));
+        setCardItem({ items: data });
+      });
+    } catch (error) {
+      // Handle any potential errors here
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  fetchItemData();
+
+  console.log(cardItem.items);
   const items = [
     {
       fav: props.fav,
--- src/components/HomeScreenComponent/MainCards.jsx
+++ src/components/HomeScreenComponent/MainCards.jsx
@@ -4,9 +4,6 @@ import Icon from "./ImagePath";
 import { useNavigate } from "react-router-dom";
 import { useStateValue } from "../../context/stateProvider";
 import { AppContext } from "../../context/appContext";
-import { collection, getDocs } from "firebase/firestore";
-import { ref } from "firebase/storage";
-import { db } from "../../firebase";
 
 const MainCards = (props) => {
   const [favItem, setFavItem] = useState(false);
@@ -14,56 +11,7 @@ const MainCards = (props) => {
   const navigate = useNavigate();
   const [{}, dispatch] = useStateValue();
   const { setItem, item } = useContext(AppContext);
-  const [cardVendor, setCardVendor] = useState([]);
-  const [cardItem, setCardItem] = useState([]);
 
-  const cardRef = collection(db, "vendor");
-  const itemRef = collection(db, "item");
-
-  const fetchVendorData = async () => {
-    try {
-      getDocs(cardRef).then((snapshot) => {
-        const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          name: doc.data().name,
-          category: doc.data().category,
-        }));
-        setCardVendor({ vendor: data });
-      });
-
-      // console.log(data);
-    } catch (error) {
-      // Handle any potential errors here
-      console.error("Error fetching data:", error);
-    }
-    console.log(cardVendor.vendor);
-  };
-
-  fetchVendorData();
-
-  const fetchItemData = async () => {
-    try {
-      getDocs(itemRef).then((snapshot) => {
-        const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          name: doc.data().name,
-          description: doc.data().description,
-          price: doc.data().price,
-          quant: doc.data().quant,
-          type: doc.data().type,
-          vid: doc.data().vid,
-        }));
-        setCardItem({ items: data });
-      });
-    } catch (error) {
-      // Handle any potential errors here
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  fetchItemData();
-
-  console.log(cardItem.items);
   const HandleCardClick = async () => {
     try {
       console.log(item);
